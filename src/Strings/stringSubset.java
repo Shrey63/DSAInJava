8bf1f0a126ef76b3d9edb8598fb123dee73cd1ac
Shreya Agrawal <shreyaparasagrawal@gmail.com>
2023-07-16 10:08:30 +0530

    String all subsets using recusrsion

--- src/Strings/stringSubset.java
+++ src/Strings/stringSubset.java
@@ -1,14 +1,27 @@
 package Strings;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class stringSubset {
     public static void main(String[] args) {
-        String s="abcdef";
+        String s="abc";
 
 
         generate("",s);
+        ArrayList<String >ans=generateSequencially("",s);
+        Collections.sort(ans, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(),o2.length());
+            }
+        });
+        System.out.println(ans.toString());
+
     }
 
-    private static void generate(String p,String up) {
+    private static void generate(String p, String up) {
         if(up.isEmpty()) {
             System.out.println(p);
             return;
@@ -17,5 +30,22 @@ public class stringSubset {
         generate(p+c,up.substring(1));
         generate(p,up.substring(1));
 
+
     }
+
+
+    private static ArrayList<String> generateSequencially(String p,String up) {
+        if(up.isEmpty()) {
+//            System.out.println(p);
+            ArrayList<String> ar=new ArrayList<String>();
+            ar.add(p);
+            return ar;
+        }
+        char c=up.charAt(0);
+       ArrayList<String> l=generateSequencially(p+c,up.substring(1));
+        ArrayList<String> r=generateSequencially(p,up.substring(1));
+        l.addAll(r);
+        return l;
+    }
+
 }
